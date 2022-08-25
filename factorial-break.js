7ac937857cd18c50d15d400c5ad1e5d5ecfd77b9
lucy1528 <luxysm@gmail.com>
2022-08-24 22:05:02 -0500

    Ejercicios factoriales

---
+++ factorial-break.js
@@ -0,0 +1,10 @@
+let i=1;
+let num=1;
+while(i<=11){
+    if(i===11){
+        break
+    }
+    num=num*i
+    console.log(num)    
+    i++
+}
---
+++ factorial-while.js
@@ -0,0 +1,7 @@
+let i=1;
+let num=1;
+while(i<=10){    
+    num=num*i
+    console.log(num)    
+    i++
+}
